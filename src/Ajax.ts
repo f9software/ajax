e2b92583f9f0a956add16feb729ebe37ed0d4a5a
Iulian Ilea <apps@iulian.net>
2017-11-02 20:23:36 +0200

    Added default headers.

--- src/Ajax.ts
+++ src/Ajax.ts
@@ -1,8 +1,41 @@
+export type Headers = {[key: string]: string};
+
 export class Ajax {
+    private static defaultHeaders: Headers;
+
+    public static setDefaultHeaders(headers: Headers) {
+        Ajax.defaultHeaders = headers;
+    }
+
+    private headers: Headers;
+
     constructor(private method: string = 'GET', private url: string) {
+        this.headers = this.mergeHeaders({}, Ajax.defaultHeaders);
+    }
+
+    private mergeHeaders(headers, defaultHeaders): Headers {
+        const newHeaders = {};
+
+        Object.keys(defaultHeaders)
+            .forEach(header => newHeaders[header] = defaultHeaders[header]);
+
+        Object.keys(headers)
+            .forEach(header => newHeaders[header] = headers[header]);
+
+        return newHeaders;
+    }
+
+    public setHeaders(headers: Headers) {
+        this.headers = this.mergeHeaders(headers, Ajax.defaultHeaders);
+    }
+
+    public getHeaders() {
+        return this.headers;
     }
 
     send(data?: {}) {
+        const headers = {};
+
         return new Promise(
             ((resolve, reject) => {
                 const xhr = new XMLHttpRequest();
@@ -12,7 +45,9 @@ export class Ajax {
                 xhr.addEventListener('abort', e => reject('The request has been cancelled by the user.'));
 
                 xhr.open(this.method, this.url);
-                xhr.setRequestHeader('Content-Type', 'application/json');
+
+                Object.keys(this.headers)
+                    .forEach(header => xhr.setRequestHeader(header, this.headers[header]));
 
                 if (data) {
                     xhr.send(JSON.stringify(data));
